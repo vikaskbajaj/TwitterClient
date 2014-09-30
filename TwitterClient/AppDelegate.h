8ba2badf7ec2427c9e72089931e01556176a589d
Vikas Kumar Bajaj <vikaskbajaj@gmail.com>
2014-09-29 23:09:05 -0700

    Initial Commit

---
+++ TwitterClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterClient
+//
+//  Created by Vikas Kumar Bajaj on 9/29/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterClient
+//
+//  Created by Vikas Kumar Bajaj on 9/29/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
